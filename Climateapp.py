997bccb228832cc9dc2412d88069ed996f4a90f4
Yann Ulysse <ulysseyann@gmail.com>
2019-04-05 21:58:49 -0400

    code comments

--- Climateapp.py
+++ Climateapp.py
@@ -12,7 +12,7 @@ import datetime as dt
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #create engine
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite?",  connect_args={'check_same_thread': False})
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -58,17 +58,17 @@ def calc_temps(start_date, end_date):
 
 
 
-#--------------------------------
-################################
-#         FLASK SETUP          #
-################################
-#-------------------------------
+
+##############################################################################################################################################################################################
+#                                                                              FLASK SETUP                                                                                                   #
+##############################################################################################################################################################################################
+
 
 app = Flask(__name__)
 
-#--------------------------------
-#         HOME ROUTE:           #
-#--------------------------------
+#################################
+#         HOME ROUTE:           # 
+#################################
 
 @app.route("/")
 def welcome():
@@ -86,9 +86,9 @@ def welcome():
 
 
 
-#--------------------------------
+#################################
 #         STATIONS ROUTE:       #
-#--------------------------------
+#################################
 
 # This query creates a list of tuples containing stations from our "stations" table
 stations_tup_list = session.query(Station.station).all()
@@ -101,15 +101,19 @@ stations_list = []
 for row in stations_tup_list:
     stations_list.append(row[0])
 
-@app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/stations/")
 def stations():
 # Return a jsonified list of stations   
     return jsonify(stations_list)
 
 
-#--------------------------------
+
+
+
+
+#################################
 #     PRECIPITATION ROUTE:      #
-#--------------------------------
+#################################
 
 station_pre_dict = {}
 
@@ -130,85 +134,88 @@ for station in stations_list:
     for row in query:
         precipitation_dict[row[0]] = row[1]
 
-# Create a key-value pair for each station and its corresponding precipitation dictionary 
+# Create a key-value pair for a station and its corresponding precipitation dictionary 
     station_pre_dict[station] = precipitation_dict
 
-@app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation/")
 def precipitation():
 # Return a jsonified list containing a our nested dictionaries
     return jsonify([station_pre_dict])
 
 
-#----------------------------------
+
+
+
+###################################
 # TEMPERATURE OBSERVATIONS ROUTE: #
-#----------------------------------
+###################################
 
-# # This query creates a list of tuples containing (<station name>, <date>, <temperature observation>) for dates within a 
-# # year prior to last date data point.
-# year_tobs_query = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
-#                               filter(Measurement.date.between(str(year_ago_from_latest), str(latest_date_obj))).all()
-# @app.route("/api/v1.0/tobs")
-# def tobs():
+""" Instead of simply providing a list of Temperature Observations (tobs) for the previous year,
+I decided to add an intuitive structure to the data (as a list of repeated dates with temperature values 
+has less interpretive value) 
 
-# # Return a jsonified list of tuples containing (<station name>, <date>, <temperature observation>) for a year from the last data point
-#     return jsonify(year_tobs_query)
+I essentially created a dictionary with "station" keys paired with lists of Temperature Observations (tobs)
+for respective stations
 
+In essence, you still preserve the intended list structure but add a clearer understanding 
+of what each date-temperature tuple (<date>, <tobs>) pertains to """
 
+# Create an empty dictionary that will hold our key-value pairs ("station" - list ) 
 temperature_dict = {}
+
+# Use a "for loop" on our list of stations (from "stations" route)
 for station in stations_list:
+
+# This query produces a list of date-temperature tuples (<date>, <tobs>) for a specific station
     query = session.query(Measurement.date, Measurement.tobs).\
                 filter(Measurement.date.between(str(year_ago_from_latest), str(latest_date_obj))).\
                     filter(Measurement.station == station).all()
+
+# Create a ("station" - list) key-value pair for a "station" and its corresponding list of tuples
     temperature_dict[station] = query
 
-@app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/tobs/")
 def tobs():
 
-# Return a jsonified list of tuples containing (<station name>, <date>, <temperature observation>) for a year from the last data point
+# Return a jsonified list containing our temperature_dict variable
     return jsonify([temperature_dict])
 
 
 
-#--------------------------------
+
+
+
+#################################
 #       START DATE ROUTE:       #
-#--------------------------------
+#################################
 
-# This query creates a distinct list of date tuples from our "measurement" table
-distinct_dates = session.query(Measurement.date).\
-    filter(Measurement.date >= earliest_date[0]).filter(Measurement.date <= latest_date[0]).distinct()
 
-# Create an empty list that will hold date strings 
-distinct_dates_list = []
-
-# Use a "for loop" to create a list of unique date strings
-# for each tuple in distinct_dates append the first element (i.e the date string) to our distinct_dates_list
-for tup in distinct_dates:
-    distinct_dates_list.append(tup[0])
-
-# @app.route("/api/v1.0/<start>")
-# def start(start):
-#     if start in distinct_dates_list:
-#         temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-#         filter(Measurement.date >= start).all()
-#         return jsonify(temp_data)
-
-def tester(st):
-    temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= st).all()
-    return temp_data
-
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/")
 def start(start):
-    return jsonify(tester(start))
+    query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
 
-#--------------------------------
+# Return the dictionary value corresponding to the specified date
+    return jsonify(query)
+
+
+
+
+
+
+#################################
 #   (START to END) DATE ROUTE:  #
-#--------------------------------
+#################################
 
-@app.route("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/<start>/<end>/")
 def start_end(start, end):
-    if start in distinct_dates_list and end in distinct_dates_list:
-        return jsonify(calc_temps( start, end))
+    return jsonify(calc_temps( start, end))
+
+
+
+
+
+
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
